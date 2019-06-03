996030b0c0a5030660077b0bfe6806ec4ebb6e6e
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-03 17:44:47 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+
+  if self.end_with?(".")
+    true
+  else
+    false
+  end
+
   end
 
   def question?
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+    true
+  else
+    false
+    end
   end
 
   def count_sentences
+    self.split(/[.!?]+/).length
+
 
   end
 end
+
+#split the string on any and all periods, question marks and exclamation marks.
+#count the number of elements that results from that split.
